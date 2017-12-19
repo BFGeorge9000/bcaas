84d2ccc12aa1325389b4f2f8f41ca2c93d952cd3
BFGeorge9000 <georgemilberger@hotmail.com>
2017-12-18 22:48:57 -0500

    BUGFIX: Protect against undefined sightings value
    Occasionally Mongo returns an empty set for 'sightings' that match newName's values, despite successfully inserting the newName document immediately preceding. Given Mongo's concurrent nature/lack of built in transactions, I'm thinking this is the result of a race condition.
    
    This if/else logic is a temporary workaround to protect against raising exceptions (calling a function on an undefined variable) while the above suspicion is being researched.

--- index.js
+++ index.js
@@ -38,11 +38,22 @@ mongo.connect(process.env.MONGODB_URI, function(err, client) {
             console.log(err);
           }
 
+          // TODO: Research this. Sometimes Mongo returns an empty set, despite successfully
+          // inserting the newName document in the line above this block. Possible race condition?
+          // This if/else is a temporary workaround to protect.
+          if(sightings.length > 0){
+            var timesSeen = sightings.length;
+            var firstSeenOn = sightings[0].last_seen_on;
+          }else{
+            var timesSeen = 1;
+            var firstSeenOn = newName.last_seen_on;
+          }
+
           var nameSummary = {
             'first_name': newName.first_name,
             'last_name': newName.last_name,
-            'times_seen': sightings.length,
-            'first_seen_on': sightings[0].last_seen_on,
+            'times_seen': timesSeen,
+            'first_seen_on': firstSeenOn,
             'last_seen_on': newName.last_seen_on
             
           }
